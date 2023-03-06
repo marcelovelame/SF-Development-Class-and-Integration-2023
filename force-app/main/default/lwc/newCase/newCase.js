3a6f0453d5859e0af6abb07c639def2f644115a5
Marcelo Velame <marcelomsv2009@gmail.com>
2023-03-06 11:58:58 -0300

    first commit

---
+++ force-app/main/default/lwc/newCase/newCase.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+/**
+ * @author: mvelame - Salesforce
+ */
+
+export default class NewCase extends LightningElement {
+
+    handleSuccess () {
+
+        const toast = new ShowToastEvent({
+            title: 'Case Success Created',
+            message: 'Caso criado com sucesso!!! :D'
+
+        });
+        
+        this.dispatchEvent(toast);
+    }
+
+}
